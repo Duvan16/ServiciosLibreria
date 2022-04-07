1828e531749b304c7e9924d593f917de29a3ccff
Duvan16 <duvan4448@gmail.com>
2022-04-06 21:47:53 -0500

    3-PaginacionMongo

--- Servicios.api.Libreria/Controllers/LibreriaAutorController.cs
+++ Servicios.api.Libreria/Controllers/LibreriaAutorController.cs
@@ -50,5 +50,15 @@ namespace Servicios.api.Libreria.Controllers
             await _autorGenericoRepository.DeleteById(Id);
         }
 
+        [HttpPost("pagination")]
+        public async Task<ActionResult<PaginationEntity<AutorEntity>>> PostPagination(PaginationEntity<AutorEntity> pagination)
+        {
+            var resultados = await _autorGenericoRepository.PaginationByFilter(
+                pagination
+                );
+
+            return Ok(resultados);
+        }
+
     }
 }
