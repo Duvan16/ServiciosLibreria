411f4307915eef7701b703e1fb82547905c74303
Duvan16 <duvan4448@gmail.com>
2022-04-25 20:12:05 -0500

    Configuración SQL Server

--- Servicios.api.Seguridad/Startup.cs
+++ Servicios.api.Seguridad/Startup.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Servicios.api.Seguridad.Core.Entities;
+using Servicios.api.Seguridad.Core.Persistence;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +29,12 @@ namespace Servicios.api.Seguridad
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddDbContext<SeguridadContexto>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("ConexionDB"));
+            });
+
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
